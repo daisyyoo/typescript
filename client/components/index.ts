09c828959cdb4d277d3712b955eeb8cbaf09402c
Daisy Yoo <daisyhyoo@gmail.com>
2022-12-18 21:56:14 +0000

    learning about assigning primitive types

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,1 +1,13 @@
 console.log('Hello, world!');
+
+let aged = true;
+let realAge = 0;
+
+if (aged) {
+  realAge = 4;
+  // must be same primitive type that was originally assigned
+}
+
+let dogAge = realAge * 7;
+
+console.log(`${dogAge} years`);
